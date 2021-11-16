4913828c5cfaf8d725df6d49edc24ca532504430
Yeon Woo Park <ywoo1234@gmail.com>
2021-11-16 00:27:55 -0500

    Update All Movies retrieval function

--- cmd/api/movie-handlers.go
+++ cmd/api/movie-handlers.go
@@ -21,10 +21,28 @@ func (app *application) getOneMovie(w http.ResponseWriter, r *http.Request) {
 	app.logger.Println("id is", id)
 
 	movie, err := app.models.DB.Get(id)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
 
 	err = app.writeJSON(w, http.StatusOK, movie, "movie")
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
 }
 
 func (app *application) getAllMovies(w http.ResponseWriter, r *http.Request) {
+	movies, err := app.models.DB.All()
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
 
+	err = app.writeJSON(w, http.StatusOK, movies, "movies")
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
 }
